b4d87b088007e1e99cc3507e4aed400d86023e91
TranTrungTinLV <trungtintin1989@gmail.com>
2024-05-20 16:43:36 +0700

    update copde

--- monstache/transform/base.js
+++ monstache/transform/base.js
@@ -1,7 +1,21 @@
-// module.exports = function (doc) {
-//   return _.pick(doc, '_id');
-// };
-
 module.exports = function (doc) {
-  return _.pick(doc, '_id', 'name', 'code', 'details');
+  if (!doc.deleted) {
+    // Giữ lại toàn bộ trường `name` cùng các trường khác
+    return {
+      _id: doc._id,
+      name: doc.name,
+      code: doc.code,
+      detail: doc.detail,
+      images: doc.images,
+      note: doc.note,
+      category_id: doc.category_id,
+      specification: doc.specification,
+      standard: doc.standard,
+      unit: doc.unit,
+      quantity: doc.quantity,
+      createdAt: doc.createdAt,
+      updatedAt: doc.updatedAt,
+    };
+  }
+  return null; // Không index các bản ghi bị xóa mềm
 };
